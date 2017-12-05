1ab1720d2c384d2d3a0304601c5231c21407246c
ChengIHsuan <cosine12621@gmail.com>
2017-12-05 12:02:26 +0800

    roll

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,7 +1,11 @@
 package com.fju.mid2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class Func1Activity extends AppCompatActivity {
 
@@ -10,4 +14,14 @@ public class Func1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
     }
+
+    public void roll(View view){
+        int num = (int) (Math.random() * 6 + 1);
+        Log.d("MainActivity","random" + num);
+        new AlertDialog.Builder(this)
+                .setMessage(num)
+                .setNegativeButton("CLOSE", null)
+                .show();
+    }
+
 }
